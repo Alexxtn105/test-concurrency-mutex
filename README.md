f8432f17e9c8b2574461c5fb1460f7dbd5a521b8
Alexxtn105 <133600113+Alexxtn105@users.noreply.github.com>
2024-12-01 16:37:54 +0300

    Финальный вариант

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Тестовый проект с использованием mutex
+
+
+Для фильтрации вывода (подсчета количества строк, содержащих "purchased"):
+```bash
+go run main.go | grep purchased | wc -l
+```
+
+Запуск программы 10 раз:
+```bash
+for run in {1..10}; do go run main.go | grep purchased | wc -l; done
+```
--- main.go
+++ main.go
@@ -3,19 +3,24 @@ package main
 import (
 	"fmt"
 	"sync"
+	"sync/atomic"
 )
 
-func main() {
-	fmt.Println("Hello, World!")
+// Мьютекс для общего ресурса - в данном случае количества билетов. Если его не будет,
+var mutex sync.Mutex
 
-	// Доступное количество билетов
+// счетчик проданных билетов (потокобезопасный)
+var count = int32(0)
+
+func main() {
+
+	// Доступное количество билетов - ЭТО ОБЩИЙ РЕСУРС!
 	var tickets int = 500
 
 	wg := sync.WaitGroup{}
-
 	// Симулируем большое количество покупателей билетов.
 	// Для простоты будем считать, что покупатель может приобрести один билет
-	for userId := 0; userId < 2000; userId++ {
+	for userId := 0; userId < 1000; userId++ {
 
 		// добавляем единичку в wg перед запсуком ОДНОЙ горутины!
 		wg.Add(1)
@@ -28,20 +33,27 @@ func main() {
 
 	// ждем окончания работы всех горутин
 	wg.Wait()
+	fmt.Println(count)
 }
 
 // buyTicket Функция покупки билета определенным пользователем
 func buyTicket(wg *sync.WaitGroup, userId int, remainingTickets *int) {
 	// в конце сигнализируем в wg, что работа закончена
 	defer wg.Done()
+	// блокируем фрагмент кода, в котором используются общие ресурысы, с помощью mutex
+	mutex.Lock()
 
 	// Проверяем, сколько осталось билетов
 	if *remainingTickets > 0 {
 		// покупка билета
 		*remainingTickets--
+		// альтернативный счетчик проданных билетов (чтобы не считать с помощью grep)
+		atomic.AddInt32(&count, 1)
 		fmt.Printf("User %d purchased a ticket. Tickets remaining:%d\n", userId, *remainingTickets)
 	} else {
 		fmt.Printf("User %d found no ticket\n", userId)
 	}
+	// отпускаем блокировку
+	mutex.Unlock()
 
 }
